0860ba22c7f886ef166f316bbeba3a29fa55b1fa
fahim al shihab <97816146+fahimalshihab@users.noreply.github.com>
2023-04-15 03:13:47 +0600

    Add files via upload

---
+++ Basic.js
@@ -0,0 +1,40 @@
+// Declare variables
+var num = 10;
+let name = "John";
+const PI = 3.14;
+
+// Declare a function
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Use a conditional statement
+if (num > 5) {
+  console.log("The number is greater than 5.");
+} else {
+  console.log("The number is less than or equal to 5.");
+}
+
+// Use a loop
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+// Declare an array
+var arr = [1, 2, 3, 4, 5];
+
+// Declare an object
+var person = {
+  name: "John",
+  age: 30,
+  city: "New York"
+};
+
+// Access object properties
+console.log(person.name);
+
+// Call a function and store the result in a variable
+var sum = addNumbers(5, 7);
+
+// Log the result to the console
+console.log(sum);
